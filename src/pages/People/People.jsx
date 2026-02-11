c56089056e7246f8eead84ca3b4cf48b9112c726
jaeyoungchoi1 <choijy5847@naver.com>
2026-02-11 14:31:03 +0900

    intern and alumni added

--- src/pages/People/People.jsx
+++ src/pages/People/People.jsx
@@ -1,6 +1,6 @@
 import tak_image from "../../images/member_photo/tak.jpg";
 import seongyeomkim_image from "../../images/member_photo/seongyeomkim.jpg";
-//import jeehunchoi_image from "../../images/member_photo/jeehunchoi.jpg";
+import jeehunchoi_image from "../../images/member_photo/jeehunchoi.jpg";
 //import donggunlee_image from "../../images/member_photo/donggunlee.jpeg";
 import joohyoungpark_image from "../../images/member_photo/joohyoungpark.jpeg";
 import yusangyun_image from "../../images/member_photo/yusangyun.jpg";
@@ -13,6 +13,13 @@ import hyunlee_image from "../../images/member_photo/hyunlee.jpeg";
 import jaeryungchung_image from "../../images/member_photo/jaeryungchung.jpg";
 import donggunlee_image from "../../images/member_photo/donggunlee.png";
 import yoonjaeoh_image from "../../images/member_photo/yoonjaeoh.jpg";
+import jungwonpark_image from "../../images/member_photo/jwpark.png";
+import chaeyounghuh_image from "../../images/member_photo/chaeyounghuh.png";
+import seunghobaek_image from "../../images/member_photo/seunghobaek.png";
+import subincho_image from "../../images/member_photo/subincho.png";
+import jaywoong_image from "../../images/member_photo/jaywoong.png";
+import placeholder_image from "../../images/member_photo/placeholder.png";
+import yoon_image from "../../images/member_photo/yoon.png";
 
 import "./People.scss";
 
@@ -124,6 +131,44 @@ function People() {
 
       </ul>
 
+      <h3>Undergraduate Interns</h3>
+      <ul className="people_list">
+        <li>
+          <a><img src={jungwonpark_image} alt="jungwonpark_image"/></a>
+          <div className="name kr">박정원</div>
+          <div className="name">Jung Won Park</div>
+          <div className="role">Intern
+          </div>
+          <div className="website">
+            <a href="https://siwon.site" target='_blank' rel="noreferrer">website</a>
+          </div>
+        </li>
+        <li>
+          <a><img src={yoon_image} alt="yoonkim_image"/></a>
+          <div className="name kr">김윤</div>
+          <div className="name">Yoon Kim</div>
+          <div className="role">Intern
+          </div>
+        </li>
+        <li>
+          <a><img src={chaeyounghuh_image} alt="chaeyounghuh_image"/></a>
+          <div className="name kr">허채영</div>
+          <div className="name">Chaeyoung Huh</div>
+          <div className="role">Intern
+          </div>
+          <div className="website">
+            <a href="https://lirisnoir.notion.site/" target='_blank' rel="noreferrer">website</a>
+          </div>
+        </li>
+        <li>
+          <a><img src={subincho_image} alt="subincho_image"/></a>
+          <div className="name kr">조수빈</div>
+          <div className="name">Subin Cho</div>
+          <div className="role">Intern
+          </div>
+        </li>
+      </ul>
+
       <h3>Alumni</h3>
       <ul className="people_list">
         <li>
@@ -131,7 +176,7 @@ function People() {
           <div className="name kr">박주형</div>
           <div className="name">Juhyeong, Park</div>
           <div className="role">
-            MSc.<br />
+            MSc. 2024<br />
             UX Designer @ Hyundai Card
           </div>
 
@@ -140,7 +185,7 @@ function People() {
           <img src={byoungjaekim_image} alt="byoungjae_image"/>
           <div className="name kr">김병재</div>
           <div className="name">Byoungjae, Kim</div>
-          <div className="role">MSc.<br/>
+          <div className="role">MSc. 2024<br/>
             PhD Student @ KAIST
           </div>
           
@@ -149,7 +194,7 @@ function People() {
           <img src={yusangyun_image} alt="yusang_image"/>
           <div className="name kr">윤유상</div>
           <div className="name">Yoosang, Yoon</div>
-          <div className="role">MSc.<br/>
+          <div className="role">MSc. 2024<br/>
             Skilled Industrial Personnel @ PLATEER
           </div>
         </li>
@@ -157,20 +202,109 @@ function People() {
           <img src={minsunkim_image} alt="minsun_image"/>
           <div className="name kr">김민선</div>
           <div className="name">Minsun, Kim</div>
-          <div className="role">MSc.<br/>
-            Product Manager @ Korea Telecom
+          <div className="role">MSc. 2024<br/>
+            Ph.D. Student @ Virginia Tech
           </div>
         </li>
         <li>
           <img src={jinjeong_image} alt="jin_image"/>
           <div className="name kr">정진</div>
           <div className="name">Jin, Jeong</div>
-          <div className="role">MSc.</div>
+          <div className="role">MSc. 2025</div>
           <div className="email">
             tasa2000@kaist.ac.kr<br/>
             Military Duty
           </div>
         </li>
+      </ul>
+      <ul className="people_list">
+        <li>
+          <img src={seunghobaek_image} alt="seungho_image"/>
+          <div className="name kr">백승호</div>
+          <div className="name">Seungho Baek</div>
+          <div className="role">Intern 2023
+          </div>
+          <div className="website">
+            <a href="https://thestar.notion.site/e6f6b641bd8f493085e5f044182dcae3" target='_blank' rel="noreferrer">website</a>
+          </div>
+        </li>
+        <li>
+          <img src={jeehunchoi_image} alt="jeehunchoi_image"/>
+          <div className="name kr">최지헌</div>
+          <div className="name">Jee-Hun Choi</div>
+          <div className="role">Intern 2022
+          </div>
+          <div className="website">
+            <a href="https://jee-hun.notion.site" target='_blank' rel="noreferrer">website</a>
+          </div>
+        </li>
+        <li>
+          <img src={placeholder_image} alt="yutaeksong_image"/>
+          <div className="name kr">송유택</div>
+          <div className="name">Yutaek Song</div>
+          <div className="role">Intern 2022
+          </div>
+        </li>
+        <li>
+          <img src={placeholder_image} alt="taewoo_image"/>
+          <div className="name kr">김태우</div>
+          <div className="name">Taewoo Kim</div>
+          <div className="role">Intern 2022
+          </div>
+        </li>
+        <li>
+          <img src={placeholder_image} alt="yubin_image"/>
+          <div className="name kr">이유빈</div>
+          <div className="name">Yubin Lee</div>
+          <div className="role">Intern 2022
+          </div>
+        </li>
+        <li>
+          <img src={placeholder_image} alt="yoojin_image"/>
+          <div className="name kr">홍유진</div>
+          <div className="name">Yoojin Hong</div>
+          <div className="role">Intern 2023
+          </div>
+        </li>        
+        <li>
+          <img src={placeholder_image} alt="suyounlee_image"/>
+          <div className="name kr">이수연</div>
+          <div className="name">Suyoun Lee</div>
+          <div className="role">Intern 2024
+          </div>
+        </li>
+        <li>
+          <img src={placeholder_image} alt="kadijahrajavova_image"/>
+          <div className="name kr">카디자</div>
+          <div className="name">Kadijah Rajavova</div>
+          <div className="role">Intern 2025
+          </div>
+        </li>
+        <li>
+          <img src={placeholder_image} alt="maida_image"/>
+          <div className="name kr">마이다</div>
+          <div className="name">Maida Aizaz</div>
+          <div className="role">Intern 2025
+          </div>
+        </li>
+        <li>
+          <img src={placeholder_image} alt="ichae_image"/>
+          <div className="name kr">류이채</div>
+          <div className="name">Ihchae Ryu</div>
+          <div className="role">Intern 2024
+          </div>
+        </li>
+        <li>
+          <a><img src={jaywoong_image} alt="jaywoong_image"/></a>
+          <div className="name kr">정재웅</div>
+          <div className="name">Jaywoong Jeong</div>
+          <div className="role">Intern 2025
+          </div>
+          <div className="website">
+            <a href="https://www.jaywoong.me" target='_blank' rel="noreferrer">website</a>
+          </div>
+        </li>
+
       </ul>
       {/* <li>
                 <img src={jeehunchoi_image} />
